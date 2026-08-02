84074cd58fe963e422f7faff596ed8713ef0d502
Stefan Schins <sjschins@gmail.com>
2026-08-02 12:39:20 -0400

    ALZ-10: Deploy Development resource group

--- landing-zones/development/main.tf
+++ landing-zones/development/main.tf
@@ -4,3 +4,8 @@ data "azurerm_virtual_network" "hub" {
   name                = var.hub_virtual_network_name
   resource_group_name = var.connectivity_resource_group_name
 }
+
+resource "azurerm_resource_group" "development" {
+  name     = var.resource_group_name
+  location = var.location
+}
