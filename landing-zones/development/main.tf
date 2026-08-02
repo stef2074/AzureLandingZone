6a9d57ecbc9cadadb73a7c9a42424c8bf20bf41c
Stefan Schins <sjschins@gmail.com>
2026-08-02 13:35:08 -0400

    ALZ-10: Deploy Development route table

--- landing-zones/development/main.tf
+++ landing-zones/development/main.tf
@@ -29,3 +29,9 @@ resource "azurerm_network_security_group" "development" {
   location            = azurerm_resource_group.development.location
   resource_group_name = azurerm_resource_group.development.name
 }
+
+resource "azurerm_route_table" "development" {
+  name                = var.route_table_name
+  location            = azurerm_resource_group.development.location
+  resource_group_name = azurerm_resource_group.development.name
+}
