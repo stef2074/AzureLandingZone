8b11562365af1fbeeed2484a946f49106055c45c
Stefan Schins <sjschins@gmail.com>
2026-08-04 10:19:56 -0400

    ALZ-11: Deploy Log Analytics Workspace

--- platform/monitoring/main.tf
+++ platform/monitoring/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "monitoring" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_log_analytics_workspace" "monitoring" {
+  name                = var.log_analytics_workspace_name
+  location            = azurerm_resource_group.monitoring.location
+  resource_group_name = azurerm_resource_group.monitoring.name
+  sku                 = var.log_analytics_workspace_sku
+  retention_in_days   = var.log_analytics_workspace_retention_in_days
+}
