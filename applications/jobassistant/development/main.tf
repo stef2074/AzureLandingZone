e0e30d63fa8dc8c51ed171852bff174b575ff3be
Stefan Schins <sjschins@gmail.com>
2026-08-09 15:44:03 -0400

    ALZ-12: Configure JobAssistant App Service logging

--- applications/jobassistant/development/main.tf
+++ applications/jobassistant/development/main.tf
@@ -32,3 +32,21 @@ data "azurerm_log_analytics_workspace" "monitoring" {
   name                = var.log_analytics_workspace_name
   resource_group_name = var.monitoring_resource_group_name
 }
+
+resource "azurerm_monitor_diagnostic_setting" "jobassistant" {
+  name                       = "ds-jobassistant-app-service"
+  target_resource_id         = azurerm_linux_web_app.jobassistant.id
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.monitoring.id
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+}
