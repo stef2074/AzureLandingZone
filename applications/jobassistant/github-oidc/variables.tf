87f490d9573807929ad35825fa75ee085d37b3de
Stefan Schins <sjschins@gmail.com>
2026-08-10 12:42:09 -0400

    ALZ-13: Configure Azure providers for GitHub OIDC

---
+++ applications/jobassistant/github-oidc/variables.tf
@@ -0,0 +1,9 @@
+variable "tenant_id" {
+  description = "Microsoft Entra tenant ID."
+  type        = string
+}
+
+variable "development_subscription_id" {
+  description = "Azure subscription ID used for the Development subscription."
+  type        = string
+}
