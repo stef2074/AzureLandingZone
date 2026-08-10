5501895a357ec679ca9dd24c75b465ee1f9d5181
Stefan Schins <sjschins@gmail.com>
2026-08-10 15:19:17 -0400

    ALZ-13: Define JobAssistant GitHub Actions Service Principal

--- applications/jobassistant/github-oidc/main.tf
+++ applications/jobassistant/github-oidc/main.tf
@@ -1,3 +1,7 @@
 resource "azuread_application" "jobassistant_github_actions" {
   display_name = var.github_actions_application_name
 }
+
+resource "azuread_service_principal" "jobassistant_github_actions" {
+  client_id = azuread_application.jobassistant_github_actions.client_id
+}
