f9b6a021fe15de4490e668ba5b967b93fa4f094c
Stefan Schins <sjschins@gmail.com>
2026-08-10 15:33:41 -0400

    ALZ-13: Configure GitHub OIDC federation settings

--- applications/jobassistant/github-oidc/variables.tf
+++ applications/jobassistant/github-oidc/variables.tf
@@ -12,3 +12,18 @@ variable "github_actions_application_name" {
   description = "Name of the Microsoft Entra Application used by JobAssistant GitHub Actions."
   type        = string
 }
+
+variable "github_repository_owner" {
+  description = "GitHub repository owner used by the federated identity credential."
+  type        = string
+}
+
+variable "github_repository_name" {
+  description = "GitHub repository name used by the federated identity credential."
+  type        = string
+}
+
+variable "github_environment_name" {
+  description = "GitHub Environment name used by the federated identity credential."
+  type        = string
+}
