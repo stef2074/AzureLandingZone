e1823ee6f95390fc6a2bd55ae818f745c1353a85
Stefan Schins <sjschins@gmail.com>
2026-08-10 15:38:29 -0400

    ALZ-13: Define JobAssistant GitHub OIDC federated credential

--- applications/jobassistant/github-oidc/main.tf
+++ applications/jobassistant/github-oidc/main.tf
@@ -5,3 +5,12 @@ resource "azuread_application" "jobassistant_github_actions" {
 resource "azuread_service_principal" "jobassistant_github_actions" {
   client_id = azuread_application.jobassistant_github_actions.client_id
 }
+
+resource "azuread_application_federated_identity_credential" "jobassistant_github_actions" {
+  application_id = azuread_application.jobassistant_github_actions.id
+  display_name   = "github-development"
+  description    = "GitHub Actions OIDC federation for the JobAssistant development environment."
+  audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
+  subject        = "repo:${var.github_repository_owner}/${var.github_repository_name}:environment:${var.github_environment_name}"
+}
